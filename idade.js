652c7c00f22fd44d8eee25e06b65ccda5eff175f
Nicole1319 <64486678+Nicole1319@users.noreply.github.com>
2020-10-18 10:15:01 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,7 @@
+
+let nome=prompt ('Digite seu nome');
+let data=prompt ('Digite o ano em que voce nasceu');
+let ano=2020;
+    ano=2020-data;
+const idade=alert(`Ol\u00e1, seu nome \u00e9 ${nome} e voc\u00ea tem ${ano} anos`)
+
